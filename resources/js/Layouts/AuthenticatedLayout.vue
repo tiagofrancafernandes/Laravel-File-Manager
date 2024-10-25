8d204b4cc47142f2bba398145353f232796e8149
Tiago França <dev@tiagofranca.com>
2024-10-25 13:25:05 -0300

    update of fm links
    	modified:   resources/js/Layouts/AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -39,6 +39,20 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     Dashboard
                                 </NavLink>
+
+                                <a
+                                    :href="route('elfinder.index')"
+                                    :class="['inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out']"
+                                >
+                                    EL Finder Explorer
+                                </a>
+
+                                <a
+                                    :href="route('unisharp.lfm.show')"
+                                    :class="['inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out']"
+                                >
+                                    Basic file manager
+                                </a>
                             </div>
                         </div>
 
@@ -146,6 +160,20 @@ const showingNavigationDropdown = ref(false);
                         >
                             Dashboard
                         </ResponsiveNavLink>
+
+                        <a
+                            :href="route('elfinder.index')"
+                            :class="['block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out']"
+                        >
+                            EL Finder Explorer
+                        </a>
+
+                        <a
+                            :href="route('unisharp.lfm.show')"
+                            :class="['block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out']"
+                        >
+                            Basic file manager
+                        </a>
                     </div>
 
                     <!-- Responsive Settings Options -->
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -20,8 +20,11 @@ import { Head } from '@inertiajs/vue3';
                 <div
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
                 >
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in!
+                    <div
+                        v-if="$page.props.auth.user"
+                        class="p-6 text-gray-900 dark:text-gray-100"
+                    >
+                        Welcome {{ $page.props?.auth?.user?.name }}!
                     </div>
                 </div>
             </div>
